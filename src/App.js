82b077084f125258bfb555c41ce86c738a2c4513
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-07-20 15:01:06 +0300

    dynamic goals

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@
 import './App.css';
 
 function App() {
+  const goals = [
+    {
+      "id": "1",
+
+      "name": "Travel Fund - Japan",
+
+      "targetAmount": 5000,
+
+      "savedAmount": 3200,
+
+      "category": "Travel",
+
+      "deadline": "2025-12-31",
+
+      "createdAt": "2024-01-15"
+    },
+  ]
   return (
     <div className="App">
       <header>
@@ -10,10 +27,12 @@ function App() {
       </header>
       <section>
         <ol>
-          <li>New Laptop</li>
-          <li>Monitor</li>
-          <li>Phone</li>
-          <li>New Car</li>
+          {goals.map((goal) => (
+            <li key={goal.id}>{goal.name}
+            <div>progress: <progress max= '100' value={goal.savedAmount/ goal.targetAmount*100}></progress></div>
+            <div>deadline: {goal.deadline}</div>
+            </li>
+          ))}
         </ol>
       </section>
     </div>
